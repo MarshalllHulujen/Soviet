6984c40ed711a8704e4d4c6d45aa4608e882b61c
NEST-97 <NEST@Nests-iMac.local>
2023-03-24 17:06:55 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/App.js",
+    "./src/About.jsx",
+    "./src/Clubs.jsx",
+    "./src/Contents.jsx",
+    "./src/Events.jsx",
+    "./src/Home.jsx",
+    "./src/Login.jsx",
+    "./src/News.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
